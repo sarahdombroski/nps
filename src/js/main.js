d528e013e3ffa65b3b2b794869ddfca779d62db2
sarahdombroski <dom22003@byui.edu>
2025-10-30 12:25:40 -0600

    completed w07

--- src/js/main.js
+++ src/js/main.js
@@ -24,4 +24,24 @@ export async function init() {
     setInfo(parkInfoLinks);
 }
 
+function enableNavigation() {
+    const menuButton = document.querySelector('#global-nav-toggle');
+        
+    menuButton.addEventListener("click", (ev) => {
+        let target = ev.target;
+        document.querySelector('.global-nav').classList.toggle('show');
+
+        if (target.tagName != 'BUTTON') {
+            target = target.closest('button');
+        }
+        
+        if (document.querySelector('.global-nav').classList.contains('show')) {
+            target.setAttribute('aria-expanded', true);
+        } else {
+            target.setAttribute('aria-expanded', false);
+        }        
+    });
+}
+
 init();
+enableNavigation();
